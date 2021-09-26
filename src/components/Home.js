115e99317a2ea766c5a7cf10589b4b82ee0079b3
siddhant gore <siddhantgore111@gmail.com>
2021-09-26 12:45:12 +0530

    v1

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+
+function Home() {
+    return (
+        <Container>
+            
+            <Section/>
+        </Container>
+    )
+}
+
+export default Home
+
+const Container=styled.div`
+    color:red;
+    height:100vh;
+`
